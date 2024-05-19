73c7fdd079c0aabe400507d1256e57cc94efae41
giji676 <tvtengoxachidze@gmail.com>
2024-05-19 23:55:21 +0100

    Basic gcode parser comleted

--- gcodeParser.py
+++ gcodeParser.py
@@ -1,3 +1,5 @@
+import re
+
 supported_commands = {
     "G0": "Coordinated Motion at Rapid Rate",
     "G1": "Coordinated Motion at Feed Rate",
@@ -5,14 +7,11 @@ supported_commands = {
     "G91": "Incremental Distance Mode",
 }
 
-supported_axis = {
-    "X": "X axis",
-    "Y": "Y axis"
-}
+supported_axis = {"X": "X axis", "Y": "Y axis"}
 
 comment_symbols = {
     ";": "Comment everything after this symbol on that line",
-    "(": "Comment everything until ')' is used to close it"
+    "(": "Comment everything until ')' is used to close it",
 }
 
 
@@ -23,8 +22,29 @@ class GcodeObject:
         self.comment = ""
 
 
+g_ex = (
+    "G1 X83.123 Y135;asd",
+    "G1 X84.094 Y135.057 E369.38473",
+    "G1 X83.787 Y134.749 E369.40144",
+    "G1 X84.138 Y134.447 E369.41922",
+)
+
+
 def parseLine(line: str) -> GcodeObject:
     result = GcodeObject()
-    for letter in line:
-        pass
+    pattern = r"(G\d+)\s(X)(\d+(\.\d*)?)\s(Y)(\d+(\.\d*)?)\s*(;(.*))?"
+    res = re.match(pattern, line)
+    result.command = supported_commands[res.group(1)]
+    result.params = {
+        supported_axis[res.group(2)]: float(res.group(3)),
+        supported_axis[res.group(5)]: float(res.group(6)),
+    }
+    result.comment = res.group(9)
     return result
+
+
+parsed = parseLine(g_ex[0])
+
+print(parsed.command,
+      parsed.params,
+      parsed.comment)
